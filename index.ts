c1bf86450304c7f618e57f0d18bb451337b3e221
Mariusz Rajczakowski (mariocoski) <mariusz@proximie.com>
2021-06-06 16:15:14 +0100

    feat: add new express handler

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import express from "express";
 import { DEFAULT_PORT_NUMBER } from "./config";
 const app = express();
 
+app.use("/status", (_, res) => {
+  res.sendStatus(200);
+});
+
 app.use("*", (_, res) => {
   res.send({
     message: "OK",
@@ -9,5 +13,7 @@ app.use("*", (_, res) => {
 });
 
 app.listen(DEFAULT_PORT_NUMBER, () => {
-  console.log(`Server is listening on: http://localhost:${DEFAULT_PORT_NUMBER}`);
+  console.log(
+    `Server is listening on: http://localhost:${DEFAULT_PORT_NUMBER}`
+  );
 });
